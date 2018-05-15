7a610e805961d59a48f2bf4362767095eb5fe915
shcks <ra98336@uem.br>
2018-05-14 22:07:07 -0300

    add final version of exer4

--- exer4.cpp
+++ exer4.cpp
@@ -7,9 +7,9 @@
 #else
 #include <unistd.h>
 #endif
-#define pollingDelay 5000
+#define pollingDelay 8000
 using namespace std;
-int identificacao = 0;
+int identificacao = 0, semCommbustivel = 0, tempoMedioPouso = 0, tempoMedioDecolagem = 0, numPouso = 0, numDec = 0;
 
 typedef struct estrutura{
     int ID;
@@ -25,14 +25,17 @@ void inicializar(FILA *fi){
     fi->inicio = NULL;
     fi->fim = NULL;
 }
+
 int gerarID(){
     identificacao += 2;
     return identificacao;
 }
+
 int randomize(int limite){
     srand(time(NULL));
     return rand() % limite;
 }
+
 void delay(){
     #ifdef _WIN32
         Sleep(pollingDelay);
@@ -45,7 +48,7 @@ void mostrar(FILA fi){
     NO *p = fi.inicio;
     cout << "\nInicio -->[";
     while(p){
-        cout << p->ID;
+        cout << p->ID << " / " << p->combustivel;
         p = p->prox;
         if(p)
             cout << ", ";
@@ -84,75 +87,155 @@ int verificaMaiorMenor(FILA filas[], int qtdFilas, int opcao){
         return indiceMenor;
     else return indiceMaior;
 }
-int remover(FILA *fi){
+
+NO* remover(FILA *fi){
     NO *p;
-    int val;
     p = fi->inicio;
     if(!p)
-        return false;
+        return NULL;
     if(fi->inicio == fi->fim)
         fi->fim = NULL;
     fi->inicio = fi->inicio->prox;
-    val = p->horaDecolagem;
-    free(p);
-    return val;
+    return p;
 }
 
-void inserir(int id, int varTempo, FILA *fi){
-    NO *novo;
-    novo = (NO*)malloc(sizeof(NO));
-    novo->ID = id;
-    novo->combustivel = novo->horaChegada = novo->horaDecolagem = varTempo;
-    novo->prox = NULL;
-
+void inserir(NO *novo, FILA *fi){
     if(!(fi->inicio))
         fi->inicio = novo;
     else
         fi->fim->prox = novo;
+    novo->prox = NULL;
     fi->fim = novo;
 }
 
 void preencheFilasPouso(FILA filasPouso[], int qtdAvioes){
     int i, indiceFila, combustivel, id;
     for(i=0; i<qtdAvioes; i++){
+        NO *novo;
+        novo = (NO*)malloc(sizeof(NO));
         indiceFila = verificaMaiorMenor(filasPouso, 4, 1);
-        combustivel = randomize(21);
+        combustivel = randomize(20) + 1;
         id = gerarID();
-        inserir(id, combustivel, &filasPouso[indiceFila]);
+        novo->combustivel = novo->horaDecolagem = combustivel;
+        novo->ID = id;
+        inserir(novo, &filasPouso[indiceFila]);
     }
 }
 
 void preencheFilasDecolagem(FILA filasDecolagem[], int qtdAvioes){
     int i, indiceFila, id;
     for(i=0; i<qtdAvioes; i++){
+        NO *novo;
+        novo = (NO*)malloc(sizeof(NO));
         indiceFila = verificaMaiorMenor(filasDecolagem, 3, 1);
         id = gerarID();
-        inserir(id, 0, &filasDecolagem[indiceFila]);
+        novo->horaChegada = 0;
+        novo->ID = id;
+        inserir(novo, &filasDecolagem[indiceFila]);
     }
 }
 
-void verificaPousoEmergencial(FILA filasPouso[]){
-    int i, aux;
+void priorizaEmergencia(FILA *filaPouso){
+    NO *p = filaPouso->inicio, *aux;
+
+    if(p->combustivel == 1)
+        return;
+    while(true){
+            aux = remover(filaPouso);
+            inserir(aux, filaPouso);
+            if(filaPouso->inicio->combustivel == 1)
+                return;
+    }
+}
+
+void verificaEmergencia(FILA filasPouso[]){
+    int i;
     NO *p;
     for(i=0; i<4; i++){
         p = filasPouso[i].inicio;
         while(p){
-            if(p->combustivel == 1 && p != filasPouso[i].inicio){
-                while(1){
-                    aux = remover(&filasPouso[i]);
-
-                }
-            }
+            if(p->combustivel == 1)
+                priorizaEmergencia(&filasPouso[i]);
             p = p->prox;
         }
     }
-
 }
 
 void realizarPousosDecolagens(FILA filasPouso[], FILA filasDecolagem[]){
-    verificaPousoEmergencial();
+    int contadorPouDec = 0, i, aux, aux2, indiceAux;
+    NO *p;
+    verificaEmergencia(filasPouso);
+    for(i=0; i<4; i++){
+        p = filasPouso[i].inicio;
+        if(p){
+            if(p->combustivel == 1) {
+                aux = filasPouso[i].inicio->horaDecolagem - filasPouso[i].inicio->combustivel;
+                remover(&filasPouso[i]);
+                tempoMedioPouso += aux;
+                semCommbustivel++;
+                numPouso++;
+                contadorPouDec++;
+            }
+        }
+    }
+    if(contadorPouDec < 3){
+        if(contadorPouDec == 0){
+            indiceAux = verificaMaiorMenor(filasDecolagem, 3, 0);
+            if(filasDecolagem[indiceAux].inicio) {
+                tempoMedioDecolagem += filasDecolagem[indiceAux].inicio->horaChegada;
+                remover(&filasDecolagem[indiceAux]);
+                numDec++;
+                contadorPouDec++;
+            }
+        }
+    }
+    for(i=contadorPouDec; i<3; i++){
+        int flag;
+        aux2 = randomize(2);
+            if(aux2){
+                flag = 0;
+                indiceAux = verificaMaiorMenor(filasDecolagem, 3, 0);
+                if(filasDecolagem[indiceAux].inicio) {
+                    tempoMedioDecolagem += filasDecolagem[indiceAux].inicio->horaChegada;
+                    remover(&filasDecolagem[indiceAux]);
+                    numDec++;
+                }else{
+                    flag = 1;
+                }
+            }else if(!aux2 || flag){
+                indiceAux = verificaMaiorMenor(filasPouso, 4, 0);
+                if(filasPouso[indiceAux].inicio) {
+                    aux = filasPouso[indiceAux].inicio->horaDecolagem - filasPouso[indiceAux].inicio->combustivel;
+                    remover(&filasPouso[indiceAux]);
+                    tempoMedioPouso += aux;
+                    numPouso++;
+                }
+            }
+    }
 }
 
+void atualizaCombustivel(FILA filasPouso[]){
+    int i;
+    NO *p;
+    for(i=0; i<4; i++){
+        p = filasPouso[i].inicio;
+        while(p){
+            p->combustivel--;
+            p = p->prox;
+        }
+    }
+}
+
+void atualizaHoraDecolagem(FILA filasDecolagem[]){
+    int i;
+    NO *p;
+    for(i=0; i<3; i++){
+        while(p){
+            p->horaDecolagem++;
+            p = p->prox;
+        }
+    }
+}
 int main(){
     FILA filasPouso[4], filasDecolagem[3];
     int i, numAvioesDecolagem, numAvioesPouso;
@@ -161,19 +244,46 @@ int main(){
     for(i=0; i<3; i++)
         inicializar(&filasDecolagem[i]);
 
-    while(1){
+    while(true){
         numAvioesPouso = randomize(4);
         numAvioesDecolagem = randomize(4);
         preencheFilasPouso(filasPouso, numAvioesPouso);
         preencheFilasDecolagem(filasDecolagem, numAvioesDecolagem);
-
-
-
-
+        cout << "\n######### FILAS DE POUSO ANTES ##########" << endl;
+        for(i=0; i<4; i++){
+            mostrar(filasPouso[i]);
+            cout << "Fila " << i+1 << " antes !";
+        }
+        cout << "\n#########################################";
+        cout << "\n######### FILAS DE DECOLAGEM ANTES ##########" << endl;
+        for(i=0; i<3; i++){
+            mostrar(filasDecolagem[i]);
+            cout << "Fila " << i+1 << " antes !";
+        }
+        cout << "\n#########################################";
+        realizarPousosDecolagens(filasPouso, filasDecolagem);
+        cout << "\n######### FILAS DE POUSO DEPOIS ##########" << endl;
+        for(i=0; i<4; i++){
+            mostrar(filasPouso[i]);
+            cout << "Fila " << i+1 << " depois !";
+        }
+        cout << "\n#########################################";
+        cout << "\n######### FILAS DE DECOLAGEM DEPOIS ##########" << endl;
+        for(i=0; i<3; i++){
+            mostrar(filasDecolagem[i]);
+            cout << "Fila " << i+1 << " antes !";
+        }
+        cout << "\n#########################################\n";
+        if(numDec)
+            cout << "Tempo medio de tempo para decolagem: " << tempoMedioDecolagem/numDec << endl;
+        if(numPouso)
+            cout << "Tempo medio de tempo para pouso: " << tempoMedioPouso/numPouso << endl;
+        cout << "Numero de avioes que pousaram sem combustivel: " << semCommbustivel << endl;
+        atualizaCombustivel(filasPouso);
+        atualizaHoraDecolagem(filasDecolagem);
         delay();
         if(_kbhit())
             break;
     }
-
     return 0;
 }
